2a580e1a894610a0013217559b58512adeb353b4
James Slaughter <james.slaughter08@gmail.com>
2019-03-20 11:50:32 -0500

    BrewSampler to Netlify again

---
+++ build/precache-manifest.93564720a9ecefe13cb9fae709d1fd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "931e41b9c964547e6402",
+    "url": "/static/js/main.8da2f61a.chunk.js"
+  },
+  {
+    "revision": "e1d5f4235a00249c785c",
+    "url": "/static/js/2.1ac51bdd.chunk.js"
+  },
+  {
+    "revision": "931e41b9c964547e6402",
+    "url": "/static/css/main.e83a36b0.chunk.css"
+  },
+  {
+    "revision": "e1d5f4235a00249c785c",
+    "url": "/static/css/2.3062db1a.chunk.css"
+  },
+  {
+    "revision": "b4b18a3a3be88fae129d5a5c970f7d63",
+    "url": "/index.html"
+  }
+];
